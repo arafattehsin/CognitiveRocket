d8f0525da38a6adcb28bfeb16057057de78d5ce4
Arafat Tehsin <arafattehsin@hotmail.com>
2017-09-09 01:49:14 +0300

    Enhanced the AMABot with Doctor's Appointment using AdaptiveCards and Levenshtein Distance Algorithm.

--- AMABot/AMABot/Dialogs/DoctorDialog.cs
+++ AMABot/AMABot/Dialogs/DoctorDialog.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
+﻿using AdaptiveCards;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +13,19 @@ namespace AMABot.Dialogs
     [Serializable]
     public class DoctorDialog : IDialog<object>
     {
+        List<string> doctorNames = new List<string> { "Ibrahim","Saleh","Robert","Katherine","Hussain",
+                                                            "Sanderson","Michelle","Anita" };
+        string doctorName = string.Empty;
+
+        #region Event Handlers
         public async Task StartAsync(IDialogContext context)
         {
             /* Wait until the first message is received from the conversation and call MessageReceviedAsync 
              *  to process that message. */
-            var message = context.MakeMessage();
-            message.Text = "Hello! May I know who do you want to book your appointment with?";
-
-            await context.PostAsync(message);
+            SendBookApptMessage(context);
             context.Wait(this.MessageReceivedAsync);
         }
+
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
@@ -28,11 +33,119 @@ namespace AMABot.Dialogs
              *  await the result. */
             var message = await result;
 
+            doctorName = CognitiveLibrary.Utilities.LevenshteinDistance.GetClosest(doctorNames, message.Text);
+
             var appointmentMessage = context.MakeMessage();
-            appointmentMessage.Text = $"That's great! Your appointment with Dr. { message } is booked.";
+            appointmentMessage.Text = $"So, you want to meet Dr. { doctorName } right?";
 
             await context.PostAsync(appointmentMessage);
-            context.Done("dr. conversation ended.");
+            Prompt(context);
         }
+
+        private async Task SubmitMessageReceived(IDialogContext context, IAwaitable<object> result)
+        {
+            IMessageActivity message = context.Activity.AsMessageActivity();
+            var jsonresult = JsonConvert.DeserializeObject<Appointment>(message.Value.ToString());
+
+            var finalReply = context.MakeMessage();
+            finalReply.Text = $"Your appointment is successfully booked with Dr. {doctorName} at {jsonresult.time} on {jsonresult.date}";
+            await context.PostAsync(finalReply);
+            context.Done<string>("conversation ended.");
+        }
+
+#endregion
+
+        #region Helper Methods
+        void Prompt(IDialogContext context)
+        {
+            try
+            {
+                // Prompt Choice
+                PromptDialog.Confirm(
+                    context: context,
+                    resume: SelectedOption,
+                    prompt: "Please select either Yes or No.",
+                    retry: "Sorry, I did not understand what you mean by that.",
+                    attempts: 3,
+                    promptStyle: PromptStyle.Keyboard);
+            }
+            catch (TooManyAttemptsException)
+            {
+                // Log exception
+            }
+        }
+
+        private async Task SelectedOption(IDialogContext context, IAwaitable<bool> result)
+        {
+            bool selectedOption = await result;
+            if (selectedOption)
+            {
+                // Show Adaptive Card Method
+                AdaptiveCard card = new AdaptiveCard();
+                // Add text to the card.
+                card.Body.Add(new TextBlock()
+                {
+                    Text = $"Appointment with Dr. { doctorName }",
+                    Size = TextSize.Large,
+                    Weight = TextWeight.Bolder
+                });
+
+                card.Body.Add(new DateInput()
+                {
+                    Id = "date"
+                });
+
+                // Add list of choices to the card.
+                card.Body.Add(new ChoiceSet()
+                {
+                    Id = "time",
+                    Style = ChoiceInputStyle.Compact,
+                    Choices = new List<Choice>()
+                    {
+                        new Choice() { Title = "11:30", Value = "1130", IsSelected = true },
+                        new Choice() { Title = "14:00", Value = "1400" },
+                        new Choice() { Title = "16:45", Value = "1645" }
+                    }
+                });
+
+                card.Actions.Add(new SubmitAction()
+                {
+                    Title = "Submit",
+                });
+
+                // Create the attachment.
+                Attachment attachment = new Attachment()
+                {
+                    ContentType = AdaptiveCard.ContentType,
+                    Content = card
+                };
+
+                var message = context.MakeMessage();
+                message.Attachments.Add(attachment);
+                await context.PostAsync(message);
+                context.Wait(SubmitMessageReceived);
+            }
+            else
+                SendBookApptMessage(context);
+        }
+
+
+        private async void SendBookApptMessage(IDialogContext context)
+        {
+            var message = context.MakeMessage();
+            message.Text = "Hello! May I know who do you want to book your appointment with?";
+
+            await context.PostAsync(message);
+        }
+
+        #endregion
     }
+
+
+    public class Appointment
+    {
+        public string date { get; set; }
+        public string time { get; set; }
+    }
+
 }
---
+++ AMABot/CognitiveLibrary/Utilities/LevenshteinDistance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CognitiveLibrary.Utilities
+{
+    public class LevenshteinDistance
+    {
+        private static int Compute(string source, string target)
+        {
+            int sourceLength = source.Length;
+            int targetLength = target.Length;
+            int[,] matrix = new int[sourceLength + 1, targetLength + 1];
+
+            // First calculation, if one entry is empty return full length
+            if (sourceLength == 0)
+                return targetLength;
+
+            if (targetLength == 0)
+                return sourceLength;
+
+            // Initialization of matrix with row size sourceLength and columns size targetLength
+            for (int i = 0; i <= sourceLength; matrix[i, 0] = i++) { }
+            for (int j = 0; j <= targetLength; matrix[0, j] = j++) { }
+
+            // Calculate rows and columns distances
+            for (int i = 1; i <= sourceLength; i++)
+            {
+                for (int j = 1; j <= targetLength; j++)
+                {
+                    int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+
+                    matrix[i, j] = Math.Min(
+                        Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+                        matrix[i - 1, j - 1] + cost);
+                }
+            }
+
+            // return result
+            return matrix[sourceLength, targetLength];
+        }
+
+        public static string GetClosest(List<string> stringsToCheck, string stringtoMatch)
+        {
+            Dictionary<string, int> resultset = new Dictionary<string, int>();
+            foreach (string s in stringsToCheck)
+            {
+                resultset.Add(s, LevenshteinDistance.Compute(stringtoMatch,s));
+            }
+            
+            //get the minimum number of modifications needed to arrive at the basestring
+            int minimumModifications = resultset.Min(c => c.Value);
+
+            //gives you a list with all strings that need a minimum of modifications to become the
+            //same as the stringtoMatch
+            var closestlist = resultset.Where(c => c.Value == minimumModifications).FirstOrDefault();
+
+            return closestlist.Key;
+        }
+    }
+}
