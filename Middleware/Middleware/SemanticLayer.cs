9d835b4d658b04760d45616aabe8134399e2bd4e
Arafat Tehsin <arafattehsin@hotmail.com>
2018-05-22 14:43:20 +0300

    Added the middleware capability to the BotFramework SDK 4

---
+++ Middleware/Middleware/SemanticLayer.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Schema;
+using Microsoft.Rest;
+using System.Collections.Generic;
+using System.Net.Http; 
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    internal class SemanticLayer : IMiddleware
+    {
+        public async Task OnTurn(ITurnContext context, MiddlewareSet.NextDelegate next)
+        {
+            if(context.Activity.Type is ActivityTypes.Message)
+            {
+                // simple middleware to add an additional send activity
+                var user = UserState<User>.Get(context);
+
+                user.Queries.Add(context.Activity.AsMessageActivity().Text);
+
+                //if (user.Queries.Count > 50)
+                //    // Send an email to the author with all respective
+                //    // output of the sentiments, like score etc. 
+                //    ExtractSentiments(user.Queries);
+            }
+            
+            await next();
+        }
+
+        private double? ExtractSentiments(List<string> Queries)
+        {
+            // Create a client.
+            ITextAnalyticsAPI client = new TextAnalyticsAPI(new ApiKeyServiceClientCredentials());
+            client.AzureRegion = AzureRegions.Westus;
+
+            MultiLanguageBatchInput multiLanguageBatchInput = new MultiLanguageBatchInput();
+            multiLanguageBatchInput.Documents = new List<MultiLanguageInput>();
+
+            for(int i = 0; i < Queries.Count; i++)
+            {
+                multiLanguageBatchInput.Documents.Add(new MultiLanguageInput("en", i.ToString(), Queries[i]));
+            }
+
+            SentimentBatchResult sentimentBatchResult = client.SentimentAsync(multiLanguageBatchInput).Result;
+            double? score = 0;
+
+            // Printing sentiment results
+            foreach (var document in sentimentBatchResult.Documents)
+                score += document.Score;
+
+            return (score / sentimentBatchResult.Documents.Count);
+        }
+    }
+
+
+    class ApiKeyServiceClientCredentials : ServiceClientCredentials
+    {
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Ocp-Apim-Subscription-Key", "adb9f24230cc469d803c448094668054");
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+
+    public class User
+    {
+        public List<string> Queries { get; set; } = new List<string>();
+        public string ID = string.Empty;
+    }
+}
---
+++ Middleware/Middleware/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using static Middleware.SemanticLayer;
+
+namespace Middleware
+{
+    public class Startup
+    {
+        private IConfiguration configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+            configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(configuration);
+            services.AddBot<CSRBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(configuration);
+                IStorage dataStore = new MemoryStorage();
+                options.Middleware.Add(new UserState<User>(dataStore));
+                options.Middleware.Add(new SemanticLayer());               
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseBotFramework();
+        }
+    }
+}
