f7dc3ce6d89a71443973830ab15c1a219608a19d
Arafat Tehsin <arafattehsin@hotmail.com>
2019-04-23 01:52:38 +0500

    Ported the library to .NET Standard 2.0 to be more standardized and compatible with other projects such as Bot Builder Community etc.

---
+++ CognitiveLibrary/SentimentAnalyzer/Sentiments.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using SentimentAnalyzer.Models;
+using System.Reflection;
+
+namespace SentimentAnalyzer
+{
+    public class Sentiments
+    {
+        public static SentimentPrediction Predict(string text)
+        {
+            //Create MLContext to be shared across the model creation workflow objects 
+            //Set a random seed for repeatable/deterministic results across multiple trainings.
+            var assembly = Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream("SentimentAnalyzer.MLModels.SentimentModel.zip");
+
+            var mlContext = new MLContext(seed: 1);
+            Sentiment statement = new Sentiment { Text = text };
+            ITransformer trainedModel = mlContext.Model.Load(stream, out var modelInputSchema);
+
+            // Create prediction engine related to the loaded trained model
+            var predEngine = mlContext.Model.CreatePredictionEngine<Sentiment, SentimentPrediction>(trainedModel);
+
+            stream.Close();
+
+            //Score
+            return predEngine.Predict(statement);
+        }
+
+    }
+}
