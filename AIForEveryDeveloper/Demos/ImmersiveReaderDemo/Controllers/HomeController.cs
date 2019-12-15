a7fd646f1a2993b14b3a3099d9716489180958be
Arafat Tehsin <arafattehsin@hotmail.com>
2019-12-16 09:14:34 +1100

    First addition to the AI For Every Developer series.

---
+++ AIForEveryDeveloper/Demos/ImmersiveReaderDemo/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ImmersiveReaderDemo.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace ImmersiveReaderDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly string TenantId;     // Azure subscription TenantId
+        private readonly string ClientId;     // Azure AD ApplicationId
+        private readonly string ClientSecret; // Azure AD Application Service Principal Client Secret
+        private readonly string Subdomain;    // Immersive Reader resource subdomain (resource 'Name' if the resource was created in the Azure portal, or 'CustomSubDomain' option if the resource was created with Azure CLI Powershell. Check the Azure portal for the subdomain on the Endpoint in the resource Overview page, for example, 'https://[SUBDOMAIN].cognitiveservices.azure.com/')
+
+        public HomeController(ILogger<HomeController> logger, Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            _logger = logger;
+            TenantId = configuration["TenantId"];
+            ClientId = configuration["ClientId"];
+            ClientSecret = configuration["ClientSecret"];
+            Subdomain = configuration["Subdomain"];
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("subdomain")]
+        public string GetSubdomain()
+        {
+            return Subdomain;
+        }
+
+        [Route("token")]
+        public async Task<string> GetToken()
+        {
+            return await GetTokenAsync();
+        }
+
+        /// <summary>
+        /// Get an Azure AD authentication token
+        /// </summary>
+        private async Task<string> GetTokenAsync()
+        {
+            string authority = $"https://login.windows.net/{TenantId}";
+            const string resource = "https://cognitiveservices.azure.com/";
+
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            ClientCredential clientCredential = new ClientCredential(ClientId, ClientSecret);
+
+            AuthenticationResult authResult = await authContext.AcquireTokenAsync(resource, clientCredential);
+
+            return authResult.AccessToken;
+        }
+    }
+}
---
+++ AIForEveryDeveloper/Demos/ImmersiveReaderDemo/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function getImmersiveReaderTokenAsync() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: '/token',
+            type: 'GET',
+            success: token => {
+                resolve(token);
+            },
+            error: err => {
+                console.log('Error in getting token!', err);
+                reject(err);
+            }
+        });
+    });
+}
+
+function getImmersiveReaderSubdomainAsync() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: '/subdomain',
+            type: 'GET',
+            success: subdomain => {
+                resolve(subdomain);
+            },
+            error: err => {
+                console.log('Error in getting subdomain!', err);
+                reject(err);
+            }
+        });
+    });
+}
