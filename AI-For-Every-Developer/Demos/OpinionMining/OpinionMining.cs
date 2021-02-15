79115e80b97aca949461456e1c8a5ae085521192
Arafat Tehsin <arafattehsin@hotmail.com>
2021-02-15 11:53:08 +1100

    feat: added the new Azure Function for Opinion Mining.

---
+++ AI-For-Every-Developer/Demos/OpinionMining/OpinionMining.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Azure.AI.TextAnalytics;
+using Azure;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpinionMining
+{
+    public static class OpinionMining
+    {
+        [FunctionName("OpinionMining")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            TextAnalyticsClient client = GetTextAnalyticsClient();
+            string sentence = req.Query["sentence"];
+
+            var sentiment = AnalyzeSentimentAsync(client, sentence, includeOpinionMining: true).Result;
+            
+            List<OpinionDTO> Opinions = new List<OpinionDTO>();
+            var opinions = sentiment?.Sentences.SelectMany(s => s.MinedOpinions);
+            if (opinions != null && opinions.Any())
+            {
+                var minedOpinionList = opinions.Select(om => new OpinionDTO()
+                {
+                    Aspect = om.Aspect.Text,
+                    Opinions = string.Join(", ", om.Opinions.Select(o => $"{o.Text} ({o.Sentiment.ToString("G")})"))
+                });
+                Opinions.AddRange(minedOpinionList);
+            }
+
+            var jsonResult = new
+            {
+                Sentiment = sentiment?.Sentiment.ToString("G"),
+                Opinions = Opinions
+            };
+
+            return new OkObjectResult(jsonResult);
+        }
+
+        private static TextAnalyticsClient GetTextAnalyticsClient()
+        {
+            var apiKey = System.Environment.GetEnvironmentVariable("API_KEY", EnvironmentVariableTarget.Process);
+            var endpointURL = System.Environment.GetEnvironmentVariable("ENDPOINT_URL", EnvironmentVariableTarget.Process);
+
+            var credentials = !string.IsNullOrEmpty(apiKey) ? new AzureKeyCredential(apiKey) : null;
+            var endpoint = !string.IsNullOrEmpty(endpointURL) && Uri.IsWellFormedUriString(endpointURL, UriKind.Absolute) ? new Uri(endpointURL) : null;
+
+            if (apiKey != null && endpointURL != null)
+                return new TextAnalyticsClient(endpoint, credentials);
+            else
+                return null;
+        }
+
+        public static async Task<DocumentSentiment> AnalyzeSentimentAsync(TextAnalyticsClient client, string input, string language = "en", bool includeOpinionMining = false)
+        {
+            var options = new AnalyzeSentimentOptions() { IncludeOpinionMining = includeOpinionMining } ;
+            return await client.AnalyzeSentimentAsync(input, language, options);
+        }
+
+        public class OpinionDTO
+        {
+            public string Aspect { get; set; }
+            public string Opinions { get; set; }
+        }
+    }
+}
+
---
+++ AI-For-Every-Developer/Demos/OpinionMining/README.md
@@ -0,0 +1,3 @@
+## Boost your Customer Service with Opinion Mining
+
+This directory is the reference for the [blog post](https://www.arafattehsin.com/customer-service-with-opinion-mining) as a part of my [AI for Every Developer](http://www.arafattehsin.com/tag/ai-for-every-developer/) series. 
